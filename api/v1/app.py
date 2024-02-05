b7adb6dcf0fc4ba1635bb2f224a5c096ae20dc75
amch-med23 <skyied.med2023@gmail.com>
2024-02-05 02:16:22 +0100

    Task 5: adding the make_response for 404 value (Not found)

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
+@app.errorhandler(404)
+def page_note_found(error):
+    """Handles the 404 error """
+    return make_response(jsonify({"error":"Not found"}), 404)
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
